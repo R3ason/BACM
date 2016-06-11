606d91e62e6f33671f0e16f30f73ac8db6a1ba11
R3ason <prater.ben@me.com>
2016-06-10 22:45:57 -0600

    upping the hipster level

--- public/scripts/hipster-library.js
+++ public/scripts/hipster-library.js
@@ -3,6 +3,8 @@
 
 var HipsterDictionary = (function(Window, undefined ){
 
+    var beerApiUrl = 'http://apis.mondorobot.com/beers/';
+    
     var partOfSpeech = {
         "noun": [
             'hop_varieties',
@@ -18,10 +20,19 @@ var HipsterDictionary = (function(Window, undefined ){
 
     var templates = [
         "This {name} is so {adjective}",
-        "The {adjective} qualities of this beer make it totally {adjective}"
+        "The {adjective} qualities of this {name} make it totally {adjective}"
     ];
 
     function sentencePolisher(template, beerProperties) {
+        //Insert Beer Name
+        template = template.replace('{name}', beerProperties.name);
+
+        //Populate Adjectives
+
+        //Build Adjective List
+        var adjectiveList = [];
+        //for(var i = 0; i < )
+        template = template;
         return template;
     }
 
@@ -30,14 +41,57 @@ var HipsterDictionary = (function(Window, undefined ){
         var templateIndex = Math.floor(Math.random() * tCount) + 1;
         var selectedTemplate = templates[templateIndex - 1];
         var beerProperties = {
-            id:"ipa"
+            id:"ipa",
+            name: "IPA"
         };
-        var sentence = sentencePolisher(selectedTemplate, beerProperties)
-        return templates[templateIndex - 1];
+        var finalSentence = sentencePolisher(selectedTemplate, beerProperties)
+        return finalSentence;
     };
 
+    function init()
+    {
+        cacheElements();
+        var href = $(this).attr("href");
+        var beerId = href.substr(href.lastIndexOf('/') + 1);
+        callBeerApi(beerId, populatePageAssets);
+    }
+    
+    function cacheElements() {
+        this.beerName = $(this).find('#beerName');
+        this.beerLabel = $(this).find('#beerLabel');
+        this.beerStyle = $(this).find('#beerStyle');
+        this.abv = $(this).find('#beerName');
+        this.description = $(this).find('#description');
+        this.generatedDescription = $(this).find('#generatedDescription');
+        this.location = $(this).find('#location');
+    }
+
+    function callBeerApi(beerId, callback) {
+        $.ajax({
+            method: "get",
+            url: beerApiUrl + beerId,
+            dataType: "json"
+        })
+        //.done(callback(data));
+        .done(function(data)
+        {
+
+        });
+    }
+
+    function populatePageAssets(data)
+    {
+        this.beerName.text(data.name);
+        this.beerLabel.attr('src', data.label_image.original);
+        this.beerStyle.text(data.style);
+        this.abv.text(data.abv);
+        this.description.text(data.description);
+    }
     return {
-        GenerateDescription: generateDescription
+        GenerateDescription: generateDescription,
+        Init: init
     };
 })(window);
+
+HipsterDictionary.Init();
 
