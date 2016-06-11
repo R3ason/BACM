b54a04d01c67ca462efe9d215131128d609be29b
paul.bauder <drawluap@gmail.com>
2016-06-11 11:22:52 -0600

    syntax error, console.log()

--- controllers/locator.js
+++ controllers/locator.js
@@ -5,13 +5,12 @@ var locatorController = {
 	locator: function(req, res) {
 		var allbeer = {}, beer = {}, id = '';
 		request('http://apis.mondorobot.com/beers/', function (error, allResponse, allBody) {
-			if (!error && response.statusCode == 200) {
+			if (!error && allResponse.statusCode == 200) {
 				allbeer = allBody;
 
 				if(req.params.id !== ''){
 					request('http://apis.mondorobot.com/beers/' + req.params.id, function(err, beerResp, beerBody){
 						beer = parseJson(beerBody).beer;
-						console.log('beer:\n',beer.description,beer.name);
 
 						res.render('locator', { 
 							id: req.params.id,
